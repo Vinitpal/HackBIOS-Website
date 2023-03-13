c95b0ef032301c3ddf22bd42ef3a360465d43253
Vinitpal <vs361017@gmail.com>
2023-03-13 19:56:02 +0530

    Speaker section

--- script/index.js
+++ script/index.js
@@ -454,3 +454,33 @@ for (var i = 0; i < TL_switch.length; i++) {
     }
   });
 }
+
+var swiper = new Swiper(".mySwiper", {
+  direction: "horizontal",
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 10,
+  loop: true,
+  loopFillGroupWithBlank: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 50,
+    },
+  },
+});
